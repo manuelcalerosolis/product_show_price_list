b9741d15108f764482c4bdf5a85e159b8412217a
dani-ddl <dani--ef-@hotmail.com>
2019-12-05 12:32:06 +0100

    añadido t03 y cambiado el orden en la vista

--- models/product.py
+++ models/product.py
@@ -10,19 +10,19 @@ _logger = logging.getLogger(__name__)
 class ProductTemplate(models.Model):
     _inherit = 'product.template'
 
-    pricelist_pub = fields.Float('First pricelist', compute='_compute_price_list', store=False,
+    pricelist_1 = fields.Float('First pricelist', compute='_compute_price_list', store=False,
                                digits=dp.get_precision('Product Price'))
-    pricelist_t12 = fields.Float('Second pricelist', compute='_compute_price_list', store=False,
+    pricelist_2 = fields.Float('Second pricelist', compute='_compute_price_list', store=False,
                                digits=dp.get_precision('Product Price'))
-    pricelist_t10 = fields.Float('Third pricelist', compute='_compute_price_list', store=False,
+    pricelist_3 = fields.Float('Third pricelist', compute='_compute_price_list', store=False,
                                digits=dp.get_precision('Product Price'))
-    pricelist_t06 = fields.Float('Fourth pricelist', compute='_compute_price_list', store=False,
+    pricelist_4 = fields.Float('Fourth pricelist', compute='_compute_price_list', store=False,
                                digits=dp.get_precision('Product Price'))
-    pricelist_t04 = fields.Float('Fifth pricelist', compute='_compute_price_list', store=False,
+    pricelist_5 = fields.Float('Fifth pricelist', compute='_compute_price_list', store=False,
                                digits=dp.get_precision('Product Price'))
-    pricelist_t01 = fields.Float('Sixth pricelist', compute='_compute_price_list', store=False,
+    pricelist_6 = fields.Float('Sixth pricelist', compute='_compute_price_list', store=False,
                                digits=dp.get_precision('Product Price'))
-    pricelist_t03 = fields.Float('Seventh pricelist', compute='_compute_price_list', store=False,
+    pricelist_7 = fields.Float('Seventh pricelist', compute='_compute_price_list', store=False,
                                digits=dp.get_precision('Product Price'))
 
     def _compute_price_list(self):
@@ -30,13 +30,13 @@ class ProductTemplate(models.Model):
         for product in self:
             _logger.info(product.item_ids)
 
-            product.pricelist_pub = 0
-            product.pricelist_t12 = 0
-            product.pricelist_t10 = 0
-            product.pricelist_t06 = 0
-            product.pricelist_t04 = 0
-            product.pricelist_t01 = 0
-            product.pricelist_t03 = 0
+            product.pricelist_1 = 0
+            product.pricelist_2 = 0
+            product.pricelist_3 = 0
+            product.pricelist_4 = 0
+            product.pricelist_5 = 0
+            product.pricelist_6 = 0
+            product.pricelist_7 = 0
 
             for product_item_id in product.item_ids:
 
@@ -45,25 +45,25 @@ class ProductTemplate(models.Model):
                 _logger.info(product_item_id.pricelist_id.id)
 
                 if product_item_id.pricelist_id.name == 'Tarifa pública':
-                    product.pricelist_pub = product_item_id.fixed_price
+                    product.pricelist_1 = product_item_id.fixed_price
 
                 if product_item_id.pricelist_id.name == 'T-12: TARIFA ALMACEN':
-                    product.pricelist_t12 = product_item_id.fixed_price
+                    product.pricelist_2 = product_item_id.fixed_price
 
                 if product_item_id.pricelist_id.name == 'T-10: TARIFA HUELVA':
-                    product.pricelist_t10 = product_item_id.fixed_price
+                    product.pricelist_3 = product_item_id.fixed_price
 
                 if product_item_id.pricelist_id.name == 'T-06: TARIFA PROFESIONALES':
-                    product.pricelist_t06 = product_item_id.fixed_price
+                    product.pricelist_4 = product_item_id.fixed_price
 
                 if product_item_id.pricelist_id.name == 'T-04: TARIFA PETIT&ASTUTO':
-                    product.pricelist_t04 = product_item_id.fixed_price
+                    product.pricelist_5 = product_item_id.fixed_price
 
                 if product_item_id.pricelist_id.name == 'T-01: TARIFA TIENDA':
-                    product.pricelist_t01 = product_item_id.fixed_price
+                    product.pricelist_6 = product_item_id.fixed_price
 
                 if product_item_id.pricelist_id.name == 'T-03: TARIFA MARIO FIALHO':
-                    product.pricelist_t03 = product_item_id.fixed_price
+                    product.pricelist_7 = product_item_id.fixed_price
 
 
 
